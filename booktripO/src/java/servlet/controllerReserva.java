89c7e62ce897acab02bffc2bf099504cc282d423
gabrielbernaldk <55105531+gabrielbernaldk@users.noreply.github.com>
2021-02-10 18:40:35 -0500

    cambios crud en reservas

--- booktripO/src/java/servlet/controllerReserva.java
+++ booktripO/src/java/servlet/controllerReserva.java
@@ -5,31 +5,26 @@
  */
 package servlet;
 
-import com.sun.xml.fastinfoset.stax.events.Util;
-import controlador.inmueblesDAO;
-import controlador.solicitudinmueblesDAO;
+import controlador.reservasDAO;
 import java.io.IOException;
 import java.io.PrintWriter;
-import static java.lang.System.out;
-import java.util.ArrayList;
 import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import modelo.inmuebles;
-import modelo.solicitudinmuebles;
+import modelo.reservas;
 
 /**
  *
  * @author gabob
  */
-@WebServlet(name = "controllerSolicitud", urlPatterns = {"/controllerSolicitud"})
-public class controllerSolicitud extends HttpServlet {
+@WebServlet(name = "controllerReserva", urlPatterns = {"/controllerReserva"})
+public class controllerReserva extends HttpServlet {
 
-    solicitudinmuebles p=new solicitudinmuebles();
-    solicitudinmueblesDAO dao=new solicitudinmueblesDAO();
+   reservas p=new reservas();
+    reservasDAO dao=new reservasDAO();
     
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
@@ -38,68 +33,78 @@ public class controllerSolicitud extends HttpServlet {
          String accion=request.getParameter("accion");
         switch (accion) {
             case "Listar":
-                List<solicitudinmuebles>lista=dao.listar();
+                List<reservas>lista=dao.listar();
                 request.setAttribute("lista", lista);
-                request.getRequestDispatcher("vista/Dashboard/listaSolicitud.jsp").forward(request, response);
+                request.getRequestDispatcher("vista/Viajero/listaReserva.jsp").forward(request, response);
                 break;
             case "Nuevo":                
-                request.getRequestDispatcher("vista/Dashboard/solicitud/add.jsp").forward(request, response);
+                request.getRequestDispatcher("vista/Viajero/reserva/add.jsp").forward(request, response);
                 break;
             case "Guardar":
                 int inmueble=Integer.parseInt(request.getParameter("txtInmueble"));
                 int usuario=Integer.parseInt(request.getParameter("txtUsuario"));
                 int estado=Integer.parseInt(request.getParameter("txtEstado"));
-                String fecha=request.getParameter("txtFecha");
-                
+                String fechaRes=request.getParameter("txtFechaReserva");
+                 String checkin=request.getParameter("txtCheckIn");
+                  String checkout=request.getParameter("txtCheckOut");
+                   p.setIdUsuario(usuario);
                 p.setIdInmueble(inmueble);
-                p.setIdUsuario(usuario);
                 p.setIdEstado(estado);
-                p.setFecha(fecha);
+                p.setFechaReserva(fechaRes);
+                 p.setCheckIn(checkin);
+                  p.setCheckOut(checkout);
                dao.agregar(p);
  
-                request.getRequestDispatcher("controllerSolicitud?accion=Listar").forward(request, response);
+                request.getRequestDispatcher("controllerReserva?accion=Listar").forward(request, response);
                 break;
             case "Editar": 
                 int ide=Integer.parseInt(request.getParameter("id"));
-                solicitudinmuebles res=dao.listarId(ide);
+                reservas res=dao.listarId(ide);
                 request.setAttribute("dato",res);
-                request.getRequestDispatcher("vista/Dashboard/solicitud/edit.jsp").forward(request, response);
+                request.getRequestDispatcher("vista/Viajero/reserva/edit.jsp").forward(request, response);
                 break;
             case "Actualizar":   
                 int id=Integer.parseInt(request.getParameter("id"));
-                int inmueble1=Integer.parseInt(request.getParameter("txtInmueble"));
-                int usuario1=Integer.parseInt(request.getParameter("txtUsuario"));
-                int estado1=Integer.parseInt(request.getParameter("txtEstado"));
-                String fecha1=request.getParameter("txtFecha");
-                 p.setIdSolicitud(id);
-                p.setIdInmueble(inmueble1);
-                p.setIdUsuario(usuario1);
-                p.setIdEstado(estado1);
-                p.setFecha(fecha1);
+               int inmueble2=Integer.parseInt(request.getParameter("txtInmueble"));
+                int usuario2=Integer.parseInt(request.getParameter("txtUsuario"));
+                int estado2=Integer.parseInt(request.getParameter("txtEstado"));
+                String fechaRes2=request.getParameter("txtFechaReserva");
+                 String checkin2=request.getParameter("txtCheckIn");
+                  String checkout2=request.getParameter("txtCheckOut");
+                   p.setIdUsuario(usuario2);
+                p.setIdInmueble(inmueble2);
+                p.setIdEstado(estado2);
+                p.setFechaReserva(fechaRes2);
+                 p.setCheckIn(checkin2);
+                  p.setCheckOut(checkout2);
            
                
                 dao.update(p);
-                request.getRequestDispatcher("controllerSolicitud?accion=Listar").forward(request, response);
+                request.getRequestDispatcher("controllerReserva?accion=Listar").forward(request, response);
                 break;
             case "Delete":      
                 int idd= Integer.parseInt(request.getParameter("id"));
                 dao.delete(idd);
-                request.getRequestDispatcher("controllerSolicitud?accion=Listar").forward(request, response);
+                request.getRequestDispatcher("controllerReserva?accion=Listar").forward(request, response);
                 break;
             default:
-                request.getRequestDispatcher("controllerSolicitud?accion=Listar").forward(request, response);;
+                request.getRequestDispatcher("controllerReserva?accion=Listar").forward(request, response);;
         }
-    
-        
-        
     }
 
-    
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
-         
     }
 
     /**
@@ -111,14 +116,9 @@ public class controllerSolicitud extends HttpServlet {
      * @throws IOException if an I/O error occurs
      */
     @Override
-    @SuppressWarnings("IncompatibleEquals")
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
-        
-     
-        
-  
     }
 
     /**
